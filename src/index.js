1612e624a22c1813eb49b6c20378db4bc50ce709
Spencer Kelly <spencermountain@gmail.com>
2019-05-23 16:56:23 -0400

    small change to extend syntax

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 let weekStart = require('./input/weekStart')
-let spacetime = require('spacetime')
 
-module.exports = weekStart = spacetime.extend({
-  weekStart: () => {
-    return weekStart()
+module.exports = {
+  weekStart: function(input) {
+    input = input || this.timezone().name
+    return weekStart(input)
   }
-})
+}
--- src/input/weekStart.js
+++ src/input/weekStart.js
@@ -1,47 +1,44 @@
-const firstDay = require('../data/countries').firstDay();
-const loc = require('../data/countries').locations();
-const iana = require('../../zonefile/iana');
+const firstDay = require('../data/countries').firstDay()
+const loc = require('../data/countries').locations()
+const iana = require('../../zonefile/iana')
 
 const spacetime = require('spacetime')
 
 function getCountry(country) {
   for (let day in firstDay) {
     for (let key in firstDay[day]) {
-      if ((key === country) || (firstDay[day][key].
-        indexOf(country) !== -1)) {
-          return { day: day, country: firstDay[day][key] }
+      if (key === country || firstDay[day][key].indexOf(country) !== -1) {
+        return { day: day, country: firstDay[day][key] }
       }
     }
   }
   for (let item in loc) {
-    if (loc[item].
-        indexOf(country) !== -1) {
+    if (loc[item].indexOf(country) !== -1) {
       return { day: 'monday', location: country }
-    } 
+    }
   }
 }
 
 function getCurrent(tz) {
-  if (!tz) { 
-    return { message: `there are problems determine time zone` } 
+  if (!tz) {
+    return { message: `there are problems determine time zone` }
   }
   // searches if current tz matches with iana
   // gets country key
   for (let key in iana) {
     if (key === tz) {
       if (!iana[key].ctry && !iana[key].loc) {
-        let country = getCountry(
-            key.substr(0, key.indexOf('/')) )
-          if (country) { return country }
-        country = getCountry(
-            (key.substr(key.indexOf('/')+1)).
-            replace('_',' ') )
-          if (country) { return country }
-      }
-      else if (iana[key].loc) {
+        let country = getCountry(key.substr(0, key.indexOf('/')))
+        if (country) {
+          return country
+        }
+        country = getCountry(key.substr(key.indexOf('/') + 1).replace('_', ' '))
+        if (country) {
+          return country
+        }
+      } else if (iana[key].loc) {
         return getCountry(country)
-      }
-      else if (iana[key].ctry) {
+      } else if (iana[key].ctry) {
         return getCountry(iana[key].ctry)
       }
     }
@@ -49,18 +46,21 @@ function getCurrent(tz) {
 }
 
 function weekStart(country = '') {
-// checks function argument and sets default value
-let tz
+  // checks function argument and sets default value
+  let tz
   if (!country || typeof country !== 'string') {
     country = null
     tz = spacetime.now().tz
   }
   if (!country) {
-    return getCurrent(tz);
+    return getCurrent(tz)
   } else if (country) {
-      let firstDay = getCountry((country.toLowerCase()).trim())
-        if (firstDay) { return firstDay }
-          else { return weekStart() }
+    let first = getCountry(country.toLowerCase().trim())
+    if (first) {
+      return first
+    } else {
+      return weekStart()
+    }
   }
 }
 
